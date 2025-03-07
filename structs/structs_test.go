84f700d3a37abdc5acc9bd8cfe30d099860b089a
1CORE <1core@protonmail.com>
2025-03-07 13:31:27 +0200

    Shape interface added

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -16,25 +16,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("want %g got %g", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 5.0}
-
-		got := rectangle.Area()
-		want := 50.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, rectangle, 50.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
 	})
 }
