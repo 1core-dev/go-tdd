6f4519b0394e214bc0eb8368b9041443d3f5171c
1CORE <1core@protonmail.com>
2025-03-10 12:21:21 +0200

    Added missed comments

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -45,6 +45,7 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+// Update changes the definition of a given word.
 func (d Dictionary) Update(word, definition string) error {
 	_, err := d.Search(word)
 
