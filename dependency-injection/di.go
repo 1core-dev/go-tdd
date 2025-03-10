650db9c1ce6f2fb269b736838b9062e0fe37639f
1CORE <1core@protonmail.com>
2025-03-10 14:26:28 +0200

    Greet HTTP handler

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,10 +1,22 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(w *bytes.Buffer, name string) {
+// Greet sends a personalized greeting to writer.
+func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+// GreeterHandler says Hello, world over HTTP.
+func GreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5050", http.HandlerFunc(GreetHandler)))
 }
