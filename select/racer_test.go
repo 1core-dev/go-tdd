b48994cadbe4b4ebbd1741c031d31b4250771221
1CORE <1core@protonmail.com>
2025-03-11 14:23:24 +0200

    Test with testhttp server

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,32 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "https://facebook.com"
-	fastURL := "https://quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
