a76f429e82f0af05e910502560b469aa87f7a436
1CORE <1core@protonmail.com>
2025-03-12 18:14:08 +0200

    Enhance logic and test cases

--- property-based-tests/numeral.go
+++ property-based-tests/numeral.go
@@ -5,7 +5,15 @@ import "strings"
 func ConvertToRoman(number int) string {
 	var result strings.Builder
 
-	for range number {
+	for i := number; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
--- property-based-tests/numeral_test.go
+++ property-based-tests/numeral_test.go
@@ -9,8 +9,10 @@ func TestRomanNumerals(t *testing.T) {
 		Roman       string
 	}{
 		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to I", 2, "II"},
-		{"3 gets converted to I", 3, "III"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
 
 	for _, tt := range cases {
