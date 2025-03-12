7515a8fae226db501da15435fa87493f3aa88208
1CORE <1core@protonmail.com>
2025-03-12 18:55:11 +0200

    Refactor ConvertToRoman

--- property-based-tests/numeral.go
+++ property-based-tests/numeral.go
@@ -2,22 +2,29 @@ package numeral
 
 import "strings"
 
-func ConvertToRoman(number int) string {
+// ConvertToRoman converts an Arabic number to a Roman Numeral.
+func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for number > 0 {
-		switch {
-		case number > 4:
-			result.WriteString("V")
-			number -= 5
-		case number == 4:
-			result.WriteString("IV")
-			number -= 4
-		default:
-			result.WriteString("I")
-			number--
+	for _, digit := range allRomanNumerals {
+		for arabic >= digit.Value {
+			result.WriteString(digit.Symbol)
+			arabic -= digit.Value
 		}
 	}
 
 	return result.String()
+}
+
+type romanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []romanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
 }
