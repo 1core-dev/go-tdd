9a27f147d352c750f7bc5f0ceeeea8c5b55ca5f3
1CORE <1core@protonmail.com>
2025-03-12 17:56:04 +0200

    Table tests fashion

--- property-based-tests/numeral.go
+++ property-based-tests/numeral.go
@@ -1,5 +1,11 @@
 package numeral
 
 func ConvertToRoman(number int) string {
+	if number == 3 {
+		return "III"
+	}
+	if number == 2 {
+		return "II"
+	}
 	return "I"
 }
--- property-based-tests/numeral_test.go
+++ property-based-tests/numeral_test.go
@@ -3,10 +3,22 @@ package numeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Numeric     int
+		Roman       string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to I", 2, "II"},
+		{"3 gets converted to I", 3, "III"},
+	}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	for _, tt := range cases {
+		t.Run(tt.Description, func(t *testing.T) {
+			got := ConvertToRoman(tt.Numeric)
+			if got != tt.Roman {
+				t.Errorf("got %q want %q", got, tt.Roman)
+			}
+		})
 	}
 }
