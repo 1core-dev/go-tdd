4a1df96fd9d35afdf5c45b841a5766b04d96ec9a
1CORE <1core@protonmail.com>
2025-03-13 16:45:12 +0200

    Non-generic stack implemetation

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -2,9 +2,36 @@ package generics
 
 import "testing"
 
-func TestAssertFunctions(t *testing.T) {
-	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, 1, 1)
-		AssertNotEqual(t, 1, 2)
+func TestStack(t *testing.T) {
+	t.Run("test stack of integers", func(t *testing.T) {
+		intStack := StackOfInts{}
+
+		AssertTrue(t, intStack.IsEmpty())
+
+		intStack.Push(42)
+		AssertFalse(t, intStack.IsEmpty())
+
+		intStack.Push(1337)
+		value, _ := intStack.Pop()
+		AssertEqual(t, value, 1337)
+
+		value, _ = intStack.Pop()
+		AssertEqual(t, value, 42)
+		AssertTrue(t, intStack.IsEmpty())
+	})
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := StackOfStrings{}
+
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
 	})
 }
