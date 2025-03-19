37d66d274ccf5792f0908753733d1d3e1462709a
1CORE <1core@protonmail.com>
2025-03-19 12:13:44 +0200

    Initial FS storage implementation

---
+++ http-server/file_system_store_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFileSystemStore(t *testing.T) {
+	t.Run("league from reader", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "John", "Wins": 42},
+			{"Name": "Sean", "Wins": 24}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"John", 42},
+			{"Sean", 24},
+		}
+		assertLeague(t, got, want)
+	})
+}
